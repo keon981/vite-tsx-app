ede5f9cca616f683245ff0d268262e1a10c590c3
Keon <Keon.ke@everfortune.ai>
2023-08-01 09:07:42 +0800

    modify tsconfig(2)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import path from 'path';
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,10 +13,10 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, '.')
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     host: "0.0.0.0"
-  }
+  },
 })
