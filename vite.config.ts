f870edadb536e0f447acad132249fd1456730e2a
Keon <Keon.ke@everfortune.ai>
2023-11-03 14:45:59 +0800

    🔧 build: vitejs/plugin-react-swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from 'vite-tsconfig-paths'
 import eslintPlugin from "vite-plugin-eslint";
 import path from "path"
