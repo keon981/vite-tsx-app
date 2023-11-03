73071f800285a4a94ee76df9f22c9f713c64c6a2
Keon <Keon.ke@everfortune.ai>
2023-11-03 14:05:37 +0800

    🔧 build: eslint

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import eslintPlugin from "vite-plugin-eslint";
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths()
+    tsconfigPaths(),
+    eslintPlugin()
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   server: {
