bf678a6263281048b30d403c1c629b00a9fb0ae5
Keon <Keon.ke@everfortune.ai>
2023-11-30 14:15:38 +0800

    🐎 ci: remote eslintPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,12 @@ import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from 'vite-tsconfig-paths'
-import eslintPlugin from "vite-plugin-eslint";
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    eslintPlugin(),
     tsconfigPaths(),
   ],
   resolve: {
