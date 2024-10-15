d9e736052e3c91930c30d7597e32ea5316e127ba
Keon <Keon.ke@everfortune.ai>
2024-10-15 16:27:32 +0800

    📃 docs(tailwind.config.js): remove container style

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,6 @@ export default {
   ],
   theme: {
     extend: {
-      container: {
-        center: true,
-        padding: "2rem",
-        screens: {
-          "2xl": "1400px",
-        },
-      },
       borderRadius: {
         lg: 'calc(var(--radius) + 2px)',
         md: 'var(--radius)',
